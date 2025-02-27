183010a680a5e591c3539b0abcbcfadeb6c6a32b
Hania Aamir <147211103+haniaaaamir@users.noreply.github.com>
2025-02-27 13:59:33 -0500

    Added Inquirer.java

---
+++ edu/ucalgary/oop/Inquirer.java
@@ -0,0 +1,31 @@
+package edu.ucalgary.oop;
+
+public class Inquirer {
+    private String FIRST_NAME;
+    private String LAST_NAME;
+    private String INFO;
+    private String SERVICES_PHONE;
+
+    public Inquirer(String firstName, String lastName, String phone, String info) {
+        this.FIRST_NAME = firstName;
+        this.LAST_NAME = lastName;
+        this.SERVICES_PHONE = phone;
+        this.INFO = info;
+    }
+
+    public String getFirstName() {
+        return FIRST_NAME;
+    }
+
+    public String getLastName() {
+        return LAST_NAME;
+    }
+
+    public String getServicesPhoneNum() {
+        return SERVICES_PHONE;
+    }
+
+    public String getInfo() {
+        return INFO;
+    }
+}
